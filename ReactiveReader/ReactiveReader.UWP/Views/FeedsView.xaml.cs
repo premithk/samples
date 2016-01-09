97fe723210f602b23bea7e2646d98406bd5c0f15
Geoffrey Huntley <ghuntley@ghuntley.com>
2016-01-09 13:25:49 +1100

    added behaviours

--- ReactiveReader/ReactiveReader.UWP/Views/FeedsView.xaml.cs
+++ ReactiveReader/ReactiveReader.UWP/Views/FeedsView.xaml.cs
@@ -1,30 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+using ReactiveReader.Core.ViewModels;
+using ReactiveUI;
 
 namespace ReactiveReader.UWP.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class FeedsView : Page
+    public sealed partial class FeedsView : Page, IViewFor<FeedsViewModel>
     {
         public FeedsView()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+
+            this.WhenActivated(d =>
+            {
+                // behaviours
+
+                // When a user adds a new blog to the feed, automatically fetch/cache the contents of the blog.
+                this.WhenAnyObservable(x => x.ViewModel.Blogs.ItemsAdded)
+                    .Subscribe(x => x.Refresh.InvokeCommand(null));
+            });
         }
+
+        object IViewFor.ViewModel
+        {
+            get { return ViewModel; }
+            set { ViewModel = (FeedsViewModel) value; }
+        }
+
+        public FeedsViewModel ViewModel { get; set; }
     }
 }
