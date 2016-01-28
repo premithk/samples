fec95a4feef1d8cbf96eb9099b847b9f9948e0d6
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2016-01-28 23:08:48 +0100

    remove IViewFor interface since we have to use view base navigation

--- apps/ReactiveReader/ReactiveReader.UWP/Views/FeedsView.xaml.cs
+++ apps/ReactiveReader/ReactiveReader.UWP/Views/FeedsView.xaml.cs
@@ -1,10 +1,9 @@
 ﻿using Windows.UI.Xaml;
 using ReactiveReader.Core.ViewModels;
-using ReactiveUI;
 
 namespace ReactiveReader.UWP.Views
 {
-    public sealed partial class FeedsView : IViewFor<FeedsViewModel>
+    public sealed partial class FeedsView
     {
         public static readonly DependencyProperty ViewModelProperty = DependencyProperty.Register(
             "ViewModel",
@@ -23,12 +22,6 @@ namespace ReactiveReader.UWP.Views
         {
             get { return (FeedsViewModel) GetValue(ViewModelProperty); }
             set { SetValue(ViewModelProperty, value); }
-        }
-
-        object IViewFor.ViewModel
-        {
-            get { return ViewModel; }
-            set { ViewModel = (FeedsViewModel) value; }
         }
     }
 }
