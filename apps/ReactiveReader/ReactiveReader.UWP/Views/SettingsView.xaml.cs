04315d0d3befb36f11232e62c276d5cf420d771d
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2016-02-14 12:03:15 +0100

    add pages and enable basic navigation

---
+++ apps/ReactiveReader/ReactiveReader.UWP/Views/SettingsView.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ReactiveReader.UWP.Views
+{
+    public sealed partial class SettingsView
+    {
+        public SettingsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
